7865881ad20b709bc61cb9237f7045ab1dbad6a7
Desiree Foard <foard.desiree@gmail.com>
2017-10-29 13:46:50 -0600

    put in the handling. this will reset the player when they move out of bounds

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 "use strict";
-// Enemies our player must avoid
+// Enemies class
 var Enemy = function(x,y,speed) {
     //set enemy's inital location
     this.x = x;
@@ -14,9 +14,9 @@ var Enemy = function(x,y,speed) {
 Enemy.prototype.update = function(dt) {
     //update the location of the enemy (multiplying the movement by the dt parameter)
     this.x = this.x + (this.speed * dt);
-    //if the enemy moves off of the screen then move enemy back to the start (also using the dt parameter) Loop through to continue?
+    //if the enemy moves off of the screen then move enemy back to the start
         if(this.x > 505){
-            this.x = 10 * dt;
+            this.x = 0 * dt;
         }
     //TODO:handles collisions with the player
 };
@@ -31,6 +31,7 @@ var Player = function(x,y){
     //set inital location
     this.x = x;
     this.y = y;
+    //setting dt so that we are able to use it later in the player update
     this.dt = 1;
     //helper to load images
     this.sprite = 'images/char-horn-girl.png';
@@ -41,19 +42,29 @@ Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    //if player moves out of bounds then they will be place back to the start
     this.dt = dt;
+    if(this.x < 1){
+        this.x = 200;
+        this.y = 400;
+    } else if (this.x > 445 || this.y > 445){
+        this.x = 200;
+        this.y = 400;
+    }
+    //when the player.y hits zero (water) then need to call modal that will come up and then reset the game within.
 };
 
 //handleInput method for player. 
 Player.prototype.handleInput = function(keyCode) {
+    //this how the player moves, using the keyCode event listener 
     if(keyCode === 'left') {
-        this.x = this.x - (500 * this.dt);
+        this.x = this.x - (1000 * this.dt);
     } else if (keyCode === 'down'){
-        this.y = this.y + (500 * this.dt);
+        this.y = this.y + (1000 * this.dt);
     } else if (keyCode === 'right'){
-        this.x = this.x + (500 * this.dt);
+        this.x = this.x + (1000 * this.dt);
     } else if (keyCode === 'up'){
-        this.y = this.y - (500 * this.dt);
+        this.y = this.y - (1000 * this.dt);
     }
 
 };
@@ -69,7 +80,7 @@ Player.prototype.reset = function() {
 };
 
 
-// create a random function to makes sure that the speed for the enemies have random speeds
+// create a random function to makes sure the enemies all have random speeds
 function rando () {
     var randOne = Math.floor((Math.random() * 20) + 1);
     randOne = randOne < 3 ? randOne + 3 : randOne;
